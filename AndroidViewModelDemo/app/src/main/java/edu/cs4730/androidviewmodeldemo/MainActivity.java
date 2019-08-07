b58c74b0d6cffce1642cc21780934bcb80b82c76
Jim Ward <seker@uwyo.edu>
2019-08-07 10:29:20 -0600

    added new example

---
+++ AndroidViewModelDemo/app/src/main/java/edu/cs4730/androidviewmodeldemo/MainActivity.java
@@ -0,0 +1,58 @@
+package edu.cs4730.androidviewmodeldemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    myAndroidViewModel mViewModel;
+    EditText et_name;
+    TextView tv_name, tv_count;
+    Button addcount, btn_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //get the modelview.
+        mViewModel = ViewModelProviders.of(this).get(myAndroidViewModel.class);
+
+        //count fields.  Modelview doesn't have livedata here.  So everything is manual.
+        tv_count = findViewById(R.id.textView);
+        addcount = findViewById(R.id.button);
+        tv_count.setText("Count is " + mViewModel.getCount());
+        addcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.incrementCount();
+                tv_count.setText("Count is " + mViewModel.getCount());
+            }
+        });
+
+        //Name field is liveData, so we have observers to the do the work here.
+        btn_name = findViewById(R.id.button2);
+        et_name = findViewById(R.id.et_name);
+        tv_name = findViewById(R.id.tv_name);
+        mViewModel.getData().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String data) {
+                tv_name.setText("Name is " + data);
+            }
+        });
+        btn_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.setName(et_name.getText().toString());
+            }
+        });
+
+    }
+}
---
+++ AndroidViewModelDemo/app/src/main/java/edu/cs4730/androidviewmodeldemo/myAndroidViewModel.java
@@ -0,0 +1,52 @@
+package edu.cs4730.androidviewmodeldemo;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+
+/**
+ * We never pass context to a ViewModel.  If you need context, use AndroidViewModel
+ * It has a get getApplication() method built into it.  also passed to in a required constructor.
+ *
+ * great for things like room db that needs context.
+ */
+
+public class myAndroidViewModel extends AndroidViewModel {
+    int count ;
+    MutableLiveData<String> name;
+
+
+    public myAndroidViewModel(@NonNull Application application) {
+        super(application);
+        count = 0;
+        name = new MutableLiveData<String>();
+        //name.setValue("None");
+    }
+
+    String getCount() {
+        return String.valueOf(count);
+    }
+
+    void incrementCount() {
+        count++;
+    }
+
+    String getName() {
+        return name.getValue();
+    }
+    void setName(String item) {
+         name.setValue(item);
+         //We really should not be toasting from here.  But as an example it works.
+        Toast.makeText(getApplication(), "New Name", Toast.LENGTH_SHORT).show();
+    }
+
+    public LiveData<String> getData() {
+        return name;
+    }
+
+}
