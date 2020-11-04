8e21ce49ae43e091f12af40c8711e1b9db9e1f1c
Jim Ward <seker@uwyo.edu>
2020-11-04 09:09:44 -0700

    minor fixes.

--- LiveDataRoomRecyclerDemo/app/src/main/java/edu/cs4730/livedataroomrecyclerdemo/myAdapter.java
+++ LiveDataRoomRecyclerDemo/app/src/main/java/edu/cs4730/livedataroomrecyclerdemo/myAdapter.java
@@ -1,7 +1,6 @@
 package edu.cs4730.livedataroomrecyclerdemo;
 
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,8 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.fragment.app.Fragment;
 
 import java.util.List;
 
