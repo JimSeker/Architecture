185d3dba0d9956e8bf1cc4033a54f942e571de91
Jim Ward <JimSeker@users.noreply.github.com>
2023-09-28 14:39:45 -0600

    updated gradle, updated to using viewbinding.

--- RxJavaRoomDemo/app/src/main/java/edu/cs4730/rxjavaroomdemo/MainActivity.java
+++ RxJavaRoomDemo/app/src/main/java/edu/cs4730/rxjavaroomdemo/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+
+import edu.cs4730.rxjavaroomdemo.databinding.ActivityMainBinding;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Action;
@@ -16,10 +18,9 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 /**
- *
  * this example uses RxJava with the room database.
  * https://github.com/ReactiveX/RxJava
- *
+ * <p>
  * https://medium.com/google-developers/room-rxjava-acb0cd4f3757  helpful explanation.
  * this example is based off of https://github.com/googlesamples/android-architecture-components/tree/master/BasicRxJavaSample
  */
@@ -28,30 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewModelFactory mViewModelFactory;
     private ScoreViewModel mViewModel;
-
     private final CompositeDisposable mDisposable = new CompositeDisposable();
-
-    private TextView mUserName;
-    private EditText mUserNameInput, mUserScoreInput;
-    private Button mUpdateButton;
+    private ActivityMainBinding binding;
 
     private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mUserName = findViewById(R.id.user_name);
-        mUserNameInput = findViewById(R.id.user_name_input);
-        mUserScoreInput = findViewById(R.id.user_score_input);
-        mUpdateButton = findViewById(R.id.update_user);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         //I've no idea what the factory is for, but won't work without it.
         mViewModelFactory = Injection.provideViewModelFactory(this);
         //setup the pieces for the viewmodel, which the observable is setup in onStart.
         mViewModel = new ViewModelProvider(this, mViewModelFactory).get(ScoreViewModel.class);
-        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+        binding.updateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 updateUserName();
@@ -66,20 +59,18 @@ public class MainActivity extends AppCompatActivity {
         // Subscribe to the emissions of the user name from the view model.
         // Update the user name text view, at every onNext emission.
         // In case of error, log the exception.
-        mDisposable.add(mViewModel.getScore()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(new Consumer<Score>() {
-                @Override
-                public void accept(Score score) throws Exception {
-                    mUserName.setText(score.getName() + ": " + score.getScore());
-                }
-            }, new Consumer<Throwable>() {
-                @Override
-                public void accept(Throwable throwable) throws Exception {
-                    Log.e(TAG, "Unable to update username", throwable);
-                }
-            }));
+        mDisposable.add(mViewModel.getScore().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Score>() {
+                    @Override
+                    public void accept(Score score) throws Exception {
+                        binding.userName.setText(score.getName() + ": " + score.getScore());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.e(TAG, "Unable to update username", throwable);
+                    }
+                }));
     }
 
     @Override
@@ -91,26 +82,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUserName() {
-        String userName = mUserNameInput.getText().toString();
-        String userScore = mUserScoreInput.getText().toString();
+        String userName = binding.userNameInput.getText().toString();
+        String userScore = binding.userScoreInput.getText().toString();
         // Disable the update button until the user name update has been done
-        mUpdateButton.setEnabled(false);
+        binding.updateUser.setEnabled(false);
         // Subscribe to updating the user name.
         // Re-enable the button once the user name has been updated
         Log.d(TAG, "starting update.");
-        mDisposable.add(mViewModel.updateScore(userName, userScore)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(new Action() {
-                @Override
-                public void run() throws Exception {
-                    mUpdateButton.setEnabled(true);
-                }
-            }, new Consumer<Throwable>() {
-                @Override
-                public void accept(Throwable throwable) throws Exception {
-                    Log.e(TAG, "Unable to update username", throwable);
-                }
-            }));
+        mDisposable.add(mViewModel.updateScore(userName, userScore).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        binding.updateUser.setEnabled(true);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.e(TAG, "Unable to update username", throwable);
+                    }
+                }));
     }
 }
